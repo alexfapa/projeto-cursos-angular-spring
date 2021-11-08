37b70e273cd808dc45f5ff3b2fbc45d7e86f310f
Alex <alexfapa32@gmail.com>
2021-11-07 23:27:28 -0300

    Adicionando tabela exibindo dados de um array estático

--- crud-angular/src/app/courses/courses-routing.module.ts
+++ crud-angular/src/app/courses/courses-routing.module.ts
@@ -2,6 +2,7 @@ import { CoursesComponent } from './courses/courses.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   { path: '', component: CoursesComponent }
 ];
--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses: Course[] = [
+    {_id: '1', name: 'Spring Boot', category: 'Back-end'},
+    {_id: '2', name: 'Angular', category: 'Front-end'},
+    {_id: '3', name: 'Docker', category: 'DevOps'}
+  ];
+  displayedColumns = ['_id','name', 'category']; //define quais campos vão ser exibidos do array
+
+  constructor() {
+    //this.courses = [];
+  }
 
   ngOnInit(): void {
   }
